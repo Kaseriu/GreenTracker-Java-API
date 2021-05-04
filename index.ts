69322e9c1d6d921449b172b8f57e1d5efa2d2a76
Kaseriu <amatuszek@myges.fr>
2021-05-04 15:41:42 +0200

    First commit

---
+++ index.ts
@@ -0,0 +1,10 @@
+import {config} from "dotenv";
+config();
+import {DatabaseUtils} from "./database/database";
+
+async function test() {
+    const str = await DatabaseUtils.getConnection();
+    const test = await str.execute('SHOW TABLES');
+    console.log(test[0])
+}
+test();
