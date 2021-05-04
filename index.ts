944da1c80524bcb7b327b304f39380a76e3347e7
MrPuel <theogouraud77@gmail.com>
2021-05-04 18:28:50 +0200

    test commit theo

--- index.ts
+++ index.ts
@@ -5,6 +5,7 @@ import {DatabaseUtils} from "./database/database";
 async function test() {
     const str = await DatabaseUtils.getConnection();
     const test = await str.execute('SHOW TABLES');
-    console.log(test[0])
+    console.log(test[0]);
+    console.log("test commit");
 }
 test();
