408ee9987a174681977fbe8a2d2c67985aeea471
Kaseriu <amatuszek@myges.fr>
2021-05-04 19:20:52 +0200

    Création branche dev

--- index.ts
+++ index.ts
@@ -6,6 +6,5 @@ async function test() {
     const str = await DatabaseUtils.getConnection();
     const test = await str.execute('SHOW TABLES');
     console.log(test[0]);
-    console.log("test committttt");
 }
 test();
